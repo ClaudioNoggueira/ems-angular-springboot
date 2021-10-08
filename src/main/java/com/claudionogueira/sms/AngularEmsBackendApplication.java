8f9ecbfa66afeb61668d072aab2520a0e77ecf21
Claudio Nogueira <cllaudionoggueira@gmail.com>
2021-10-08 17:35:49 -0300

    Project created

---
+++ src/main/java/com/claudionogueira/sms/AngularEmsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.claudionogueira.sms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AngularEmsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AngularEmsBackendApplication.class, args);
+	}
+
+}
